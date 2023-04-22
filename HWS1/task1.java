cd783f87b92c2cc2c04b53cb7261c84609fe6882
Семён <karachinstar@yandex.ru>
2023-04-22 11:01:57 +0800

    Решение первой задачи

---
+++ HWS1/task1.java
@@ -0,0 +1,23 @@
+package HomeWork.HWS1;
+import java.util.Scanner;
+/**
+ * Вычислить n-ое треугольного число (сумма чисел от 1 до n),
+ *  n! (произведение чисел от 1 до n)
+ */
+public class task1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите число n = ");
+        int n = iScanner.nextInt();
+        iScanner.close();
+        int sumNum = 0;
+        int prodNum = 1;
+        for(int i = 1; i <= n; i++){
+         sumNum = sumNum + i;
+         prodNum = prodNum * i;
+        }
+        System.out.printf("Сумма чисел от 1 до %d = %d", n, sumNum);
+        System.out.printf("\nПроизведение чисел от 1 до %d = %d", n, prodNum);
+    }
+    
+}
