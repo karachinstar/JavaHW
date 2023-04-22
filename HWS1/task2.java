cd17f484e97b9bac48d44951248bf122ca02a1e4
Семён <karachinstar@yandex.ru>
2023-04-22 11:42:29 +0800

    Решение второй задачи

---
+++ HWS1/task2.java
@@ -0,0 +1,30 @@
+package HomeWork.HWS1;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+/**
+ * Вывести все простые числа от 1 до 1000
+ */
+public class task2 {
+    public static void main(String[] args) {
+        int n = 1000;
+        int counter = 0;
+        int counter2 = 0;
+        System.out.printf("Список простых чисел от 1 до %d:\n", n);
+        for (int i = 2; i <= n; i++){
+            for (int j = 2; j <= i; j++){
+                if(i % j == 0){
+                    counter ++;
+                }
+            }
+            if( counter == 1){
+                System.out.printf("%d  ", i);
+                counter2 ++;
+                if(counter2 % 20 == 0){
+                    System.out.println();
+                } 
+            }
+            counter = 0;
+        }
+    }
+}
